dd044a8bb54ddfbbcc8f7daeaf280c6a91413bb0
Matthew Chen <mchen146@ucsc.edu>
2023-12-25 21:27:15 -0800

    vite use env

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,10 +1,16 @@
 // import vike from 'vike/plugin';
+import path from 'node:path';
+
+import dotenv from 'dotenv';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import solidSvg from 'vite-plugin-solid-svg';
 import tsconfigPaths from 'vite-tsconfig-paths';
 // import devtools from 'solid-devtools/vite';
 
+const envPath = path.resolve(__dirname, '../.env');
+console.log('ENV resolved path:', envPath);
+dotenv.config({ path: envPath });
 
 export default defineConfig({
     plugins: [
@@ -19,7 +25,7 @@ export default defineConfig({
         // vike()
     ],
     server: {
-        port: 3000,
+        port: process.env.API_PORT as unknown as number || 3000,
     },
     build: {
         target: 'esnext',
